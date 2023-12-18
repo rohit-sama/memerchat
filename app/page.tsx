ddc639ccda87c1f2ffaa79838d7e0cef10aa6f5b
rohit-sama <rohit9804singh@gmail.com>
2023-12-18 15:11:33 +0530

    more changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 import Chat from "./conponents/chat";
 import { auth } from "@clerk/nextjs";
 import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
 
 
 export default async function Home() {
@@ -10,8 +12,8 @@ export default async function Home() {
   return (
     <div>
     {userId && <div className="h-screen bg-black ">   
-    <div className="flex justify-between fixed top-0 left-0 w-full z-10 items-center bg-transparent backdrop-blur-md border-b-2 border-gray-400 py-5 px-10">
-      <h1 className="text-green-400 text-xl">MemerChat</h1>
+    <div className="flex justify-between fixed top-0 left-0 w-full z-10 items-center bg-transparent backdrop-blur-md border-b-2 border-gray-400 py-1 px-10">
+      <h1 className="text-green-400 text-lg "><Image src="/favicon.ico" alt="imglogo" height={50} width={50} /> MemerChat</h1>
        <UserButton  afterSignOutUrl="/"/>
        </div>
       <Chat />
